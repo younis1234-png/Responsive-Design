67c8895934e889939184f3d08aa8964d75fd9e4c
Younis saleh <younissaleh@Youniss-MacBook-Pro.local>
2020-12-03 14:06:50 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const menu = document.getElementById("menu")
+const nav = document.getElementById("nav")
+const exit = document.getElementById("exit")
+
+
+menu.addEventListener("click", (e) => {
+    nav.classList.toggle("hide-mobile")
+
+    e.preventDefault()
+})
+exit.addEventListener("click", (e) => {
+    nav.classList.add("hide-mobile")
+
+    e.preventDefault()
+})
